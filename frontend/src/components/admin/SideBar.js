edefbab7dd975b909577b099d96bc66ee4d536eb
Nwakodo Ifeanyi <46559372+ify12345@users.noreply.github.com>
2023-12-10 18:36:02 +0100

    mistakes

---
+++ frontend/src/components/admin/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+      j
+    </div>
+  )
+}
+
+export default SideBar
--- frontend/src/components/admin/students/Complaints.js
+++ frontend/src/components/admin/students/Complaints.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Sidebar from '../Sidebar'
+import SideBar from '../SideBar'
+
 
 const Complaints = () => {
   return (
     <section className="flex-col flex md:flex-row justify-between w-full h-full">
-    <Sidebar/>
+    <SideBar/>
     <div className="w-full  bg-[#C8D1DA] px-6 flex flex-col gap-3">
       <h4> Students Complaints</h4>
     </div>
